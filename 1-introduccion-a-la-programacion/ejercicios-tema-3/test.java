af077ed79ed720bfce949f5809d70bd01c89b19a
Guido <guidopasciucco@gmail.com>
2023-01-05 21:47:48 -0300

    #1

---
+++ 1-introduccion-a-la-programacion/ejercicios-tema-3/test.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(10,50);
+    }
+    public static void suma(int a, int b){
+        int resultado;
+        resultado = a + b;
+        System.out.println(resultado);
+    }
+}  
